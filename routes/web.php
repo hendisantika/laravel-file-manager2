da79a4816e0caef7baa3d418b1d35e5d82f90b62
hendisantika <hendisantika@yahoo.co.id>
2024-02-04 06:12:08 +0700

    Add filemanager route🫸🌀✏️📗🐧🐳

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use UniSharp\LaravelFilemanager\Lfm;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group(['prefix' => 'filemanager', 'middleware' => ['web', 'auth']], function () {
+    Lfm::routes();
 });
